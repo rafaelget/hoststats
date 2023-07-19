92bc25887de4cf83a35f39b48056be21009ba4a3
Rafael Müller <rafael@iggy.com.br>
2023-07-19 09:36:10 -0300

    auth

--- code/index.php
+++ code/index.php
@@ -1,4 +1,32 @@
 <?php
+function authenticate($user, $pass) {
+    $users = [
+        'username' => 'password'
+    ];
+    if (isset($users[$user]) && ($users[$user] === $pass)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if ( empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW']) || !authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
+    header('WWW-Authenticate: Basic realm="Please Login"');
+    header('Content-Type: application/json; charset=utf-8');
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+
+    $response = [
+        'status'    => 0,
+        'message'   => 'Invalid credentials',
+    ];
+
+    http_response_code(401);
+    echo json_encode($response);
+    exit;
+}
+
 $path = '../count/containers.csv';
 $error = null;
 if ( file_exists($path) ){
