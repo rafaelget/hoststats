c6206ec26323f3575e50714bdcf7dc3f688a8920
Rafael Müller <rafael@iggy.com.br>
2023-07-23 12:09:45 -0300

    Update index.php

--- code/index.php
+++ code/index.php
@@ -1,5 +1,7 @@
 <?php
+header('Access-Control-Allow-Headers: *');
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json; charset=utf-8');
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
